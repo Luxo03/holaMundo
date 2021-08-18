4da25f6711170e546d4097694535cd17dde03780
Luciano <Luxo_03@hotmail.com>
2021-08-18 18:10:24 -0300

    maquetado inicial y algunos ejercicios con js

---
+++ js/holaMundo.js
@@ -0,0 +1,34 @@
+// Esto es un comentario simple o de una sola linea
+
+/* Esto es un comentario
+de multiples
+lineas */
+
+// mostrar un mensaje en consola
+console.log('Hola comision 16i');
+
+// mostrar un msj en el html
+document.write('Este es mi primer msj')
+
+// mostrar un msj en una ventana emergente
+// alert('Hola mundo');
+
+// crear una variable (var - let - const)
+
+let comision = '16i';
+ 
+// tambien reconoce codigo de lenguaje html
+
+document.write('<br>Comision: '+ comision);
+ 
+// modificar el contenido de una variable
+ 
+comision = 'i';
+ 
+document.write('<br>Comision: 16'+ comision);
+ 
+// variable constante
+ 
+const anio = 2021;
+ 
+document.write('<br>Año: '+ anio)
