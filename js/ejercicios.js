bf6b4ed4cde18a8d3e6bcd8a72c34f7c064fa842
Luciano <Luxo_03@hotmail.com>
2021-08-18 23:58:54 -0300

    ejercicios post maquetado

---
+++ js/ejercicios.js
@@ -0,0 +1,29 @@
+//Ej 1 alerta sobre 'un mensaje' ↓
+
+// alert('un mensaje')
+ 
+//Ej 2 Programa Hello World ↓
+
+let saludo = 'Hello ';
+let saludo2 = 'World';
+
+document.write(saludo + saludo2)
+ 
+//Ej 3 programa suma
+ 
+document.write('<br>Resultado: ')
+
+let x = 3;
+let y = 5;
+ 
+document.write(x+y)
+ 
+//Ej 4 solicita nombre de usuario
+ 
+//prompt ('Hola nombreUsuario')
+ 
+//Ej 5 prog 3 lineas num, num y resultado
+ 
+document.write('<br>El resultado de la suma es: ')
+let n1 = parseInt(prompt ('Ingrese un numero'))
+let n2 = parseInt(prompt ('Ingrese un numero'))
